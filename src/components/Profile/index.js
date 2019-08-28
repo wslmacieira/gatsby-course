504c1010c5a00d5dd44e401b37735e0a61ac2fbc
wslmacieira <wslmacieira@outlook.com>
2019-08-28 19:02:40 -0300

    component Profile

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => (
+    <div className="Profile-wrapper">
+        <h1>Wagner Dos Santos</h1>
+        <h2>Programador Frontend</h2>
+        <p>Membro do Core Team da Empresa DWL, escrevo sobre o mundo do frontend e outras coisas mais.</p>
+    </div>
+)
+
+export default Profile
