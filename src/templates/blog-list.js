1198cccf99919349d422b6625bbe0621a69c880e
wslmacieira <wslmacieira@outlook.com>
2019-08-30 20:35:18 -0300

    criando component de Paginação

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -4,33 +4,49 @@ import { graphql } from 'gatsby'
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import PostItem from '../components/PostItem'
+import Pagination from '../components/Pagination'
 
 const BlogList = props => {
-   const postList = props.data.allMarkdownRemark.edges
+  const postList = props.data.allMarkdownRemark.edges
 
-   return (
+  const { currentPage, numPages } = props.pageContext
+  const isFirst = currentPage === 1
+  const isLast = currentPage === numPages
+  const prevPage = currentPage - 1 === 1 ? '/' : `/page/${currentPage - 1}`
+  const nextPage = `/page/${currentPage + 1}`
+
+  return (
     <Layout>
-    <SEO title="Home" />
-    {postList.map(
-    ({
-      node: { 
-      frontmatter: { background, category, date, description, title },
-      timeToRead,
-      fields: { slug },
-      },
-      }) => (
-        <PostItem
-        slug={slug}
-        background={background}
-        category={category}
-        date={date}
-        timeToRead={timeToRead}
-        title={title}
-        description={description}
-        />
-      ))}
-  </Layout>
-   )
+      <SEO title="Home" />
+      {postList.map(
+        ({
+          node: {
+            frontmatter: { background, category, date, description, title },
+            timeToRead,
+            fields: { slug },
+          },
+        }) => (
+            <PostItem
+              slug={slug}
+              background={background}
+              category={category}
+              date={date}
+              timeToRead={timeToRead}
+              title={title}
+              description={description}
+            />
+          )
+      )}
+
+      <Pagination
+        isFirst={isFirst}
+        isLast={isLast}
+        currentPage={currentPage}
+        numPages={numPages}
+        prevPage={prevPage}
+        nextPage={nextPage} />
+    </Layout>
+  )
 }
 
 export const query = graphql`
