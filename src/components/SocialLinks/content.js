6cce5ce2ce91777260272a87773c322a215db904
wslmacieira <wslmacieira@outlook.com>
2019-08-29 23:48:53 -0300

    MenuLinks component

--- src/components/SocialLinks/content.js
+++ src/components/SocialLinks/content.js
@@ -20,3 +20,5 @@ const links = [
         url: 'https://unsplash.com'
     }
 ]
+
+export default links
--- src/components/SocialLinks/index.js
+++ src/components/SocialLinks/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from  'react'
 
 import Icons from './Icons'
 import links from './content'
@@ -6,26 +6,27 @@ import links from './content'
 import * as S from './styled'
 
 const SocialLinks = () => (
-    <S.SocialLinksWrapper />
-    <S.SocialLinksList >
-        {links.map((link, i) => {
-            const Icon = Icons[link.label]
+    <S.SocialLinksWrapper>
+        <S.SocialLinksList >
+            {links.map((link, i) => {
+                const Icon = Icons[link.label]
 
-            return (
-                <S.SocialLinksItem key={i}>
-                    <S.SocialLinksLink
-                        href={link.url}
-                        title={link.label}
-                        target="_blank"
-                        rel="noopener noreferrer" >
+                return (
+                    <S.SocialLinksItem key={i}>
+                        <S.SocialLinksLink
+                            href={link.url}
+                            title={link.label}
+                            target="_blank"
+                            rel="noopener noreferrer" >
                             <S.IconWrapper>
                                 <Icon />
                             </S.IconWrapper>
                         </S.SocialLinksLink>
-                </S.SocialLinksItem>
-            )
-        })}
-    </S.SocialLinksList>
+                    </S.SocialLinksItem>
+                )
+            })}
+        </S.SocialLinksList>
+    </S.SocialLinksWrapper>
 )
 
 export default SocialLinks
