77a535e88b85aa0d1134e45f4c59d1b1d27ba46c
wslmacieira <wslmacieira@outlook.com>
2019-09-01 15:04:08 -0300

    estilizando com css grid

--- src/components/Layout/styled.js
+++ src/components/Layout/styled.js
@@ -8,5 +8,11 @@ export const LayoutMain = styled.main`
   background: var(--background);
   min-height: 100vh;
   padding: 0 3.75rem 0 20rem;
-  width: 100%
+  width: 100%;
+
+  body#grid & {
+    grid-template-areas:
+      "posts"
+      "pagination";
+  }
 `
--- src/components/PostItem/styled.js
+++ src/components/PostItem/styled.js
@@ -7,12 +7,24 @@ export const PostItemWrapper = styled.section`
   display: flex;
   padding: 2rem 3rem;
   width: 100%;
+
+  body#grid & {
+    border: none;
+    padding: 2rem 1rem;
+    flex-direction: column;
+    justify-content: center;
+  }
 `
 
 export const PostItemLink = styled(Link)`
   color: var(--texts);
   display: flex;
   text-decoration: none;
+
+  body#grid & {
+    background-color: var(--background);
+  }
+
   &:hover {
     color: var(--highlight);
   }
@@ -30,12 +42,21 @@ export const PostItemTag = styled.div`
   min-height: 90px;
   min-width: 90px;
   text-transform: uppercase;
+
+  body#grid & {
+    margin-bottom: 1.5rem;
+  }
 `
 
 export const PostItemInfo = styled.div`
   display: flex;
   flex-direction: column;
   margin-left: 1.5rem;
+
+  body#grid & {
+    line-height: 1.1;
+    margin: 0.8rem 0;
+  }
 `
 
 export const PostItemDate = styled.time`
@@ -46,6 +67,11 @@ export const PostItemTitle = styled.h1`
   font-size: 1.6rem;
   font-weight: 700;
   margin: 0.2rem 0 0.5rem;
+
+  body#grid & {
+    line-height: 1.1;
+    margin: 0.8rem 0;
+  }
 `
 
 export const PostItemDescription = styled.p`
